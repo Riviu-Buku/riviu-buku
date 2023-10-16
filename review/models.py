fe13bbc959af8c6492fea10e06c8491d18c111eb
Samuel Taniel Mulyadi <samueltaniel@gmail.com>
2023-10-16 23:06:18 +0700

    update first draft review bikin layout

--- review/models.py
+++ review/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    reply = models.IntegerField()
+    date_added = models.DateField(auto_now_add=True)
+    description = models.TextField()
