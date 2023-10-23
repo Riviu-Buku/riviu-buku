0cdd0d6aec9a802d765bf675472eca95570e9b99
Samuel Taniel Mulyadi <samueltaniel@gmail.com>
2023-10-23 23:17:49 +0700

    dataset book beracil

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,5 +4,6 @@ from homepage.views import *
 app_name = 'homepage'
 
 urlpatterns = [
-    path('', show_homepage, name='show_homepage')
+    path('', show_homepage, name='show_homepage'),
+    path('get_books', get_books, name='get_books')
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+from homepage.models import Book
 
 def show_homepage(request):
     return render(request, 'homepage.html')
+
+# Create your views here.
+def get_books (request):
+    data = Book.objects.all()
+    return HttpResponse(serializers. serialize("json", data), content_type="application/json")
