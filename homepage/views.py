50b9b00fba305a5c0b3ae9233703c44b9159726b
KhansaMahira <khansamahira011@gmail.com>
2023-10-29 23:44:23 +0700

    test homepage

--- homepage/views.py
+++ homepage/views.py
@@ -17,8 +17,6 @@ def get_books (request):
     data = Book.objects.all()
     return HttpResponse(serializers. serialize("json", data), content_type="application/json")
 
-
-
 def get_books_by_id(request, id):
     book = Book.objects.filter(pk=id)  # Use filter to get a queryset with a single object
     if book.exists():  # Check if the book with the given ID exists
