4bdd5992947a1bddfda4b20f9fa376e4567229df
Samuel Taniel Mulyadi <samueltaniel@gmail.com>
2023-12-17 23:07:13 +0700

    finish logout dan signup

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,5 +6,6 @@ app_name = 'authentication'
 urlpatterns = [
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
-    path('login-flutter/', login_custom)
+    path('login-flutter/', login_custom),
+    path('register-flutter/', register)
 ]
